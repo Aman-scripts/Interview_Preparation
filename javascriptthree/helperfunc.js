be3f23b18876f46e6d9b489bc712f1244f70dfe6
amandeep <amandeep.nugen@gmail.com>
2026-04-23 15:44:26 +0530

    map & filter

---
+++ javascriptthree/helperfunc.js
@@ -0,0 +1,34 @@
+// map ,filter,arrow funs
+
+function sum(a,b){
+    return a+b;
+}
+const ans=sum(1,2)
+console.log(ans)
+
+const mul=(a,b)=>{
+    return a*b;
+}
+
+console.log(mul(4,5))
+
+
+
+const arr=[1,2,3,4,5]
+const res=arr.map((value)=>value*2)
+console.log(res,"value")
+
+
+const input=[2,3,4,5,6,7,8]
+const newArray=[]
+for(let i=0;i<input.length;i++){
+    newArray.push(input[i]*3)
+}
+console.log(newArray)
+
+
+//Filtering {Even numbers from array}
+
+const filterArray=[1,2,3,4,5,6,7]
+const result=filterArray.filter((value)=>value%2==0)
+console.log(result)
--- javascripttwo/removeduplicateid.js
+++ javascripttwo/removeduplicateid.js
@@ -17,3 +17,4 @@ function duplicateById(value) {
 }
 let data = [{ id: 1, name: "A" }, { id: 2, name: "B" }, { id: 1, name: "A" }]
 console.log(duplicateById(data))
+
